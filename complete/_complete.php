1745cb1e551113a4ed81a21a7cf6ea1f458ce2e0
matsui <matsui52@gmail.com>
2016-07-06 23:35:23 +0900

    add complete page

--- complete/_complete.php
+++ complete/_complete.php
@@ -1,2 +1,66 @@
-<?php
-  echo 'Congratulations!(Test)';
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex,nofollow">
+    <title>ARコンプリートおめでとう! | WordCamp Kansai 2016</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="./css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <header id="header">
+      <div class="mv">
+        <h1 class="mv-tit">コンプリートおめでとう！</h1>
+      </div>
+    </header>
+
+    <div class="container">
+      <p>1F アセンブリーホールの<strong>「企画カウンター」</strong>にいるスタッフに、この画面をお見せ下さい。</p>
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h2 class="tit-sub">＜ 交換場所 ＞</h2>
+            <img class="img-responsive center-block" src="img/map.png" alt="アセンブリーホールマップ図" title="">
+            <p class="notice">※&nbsp;時間帯により、多少レイアウトが異なります。</p>
+          </div>
+        </div>
+    </div>
+
+    <h2 class="tit">オリジナルグッズについて</h2>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-4">
+          <figure>
+            <img class="img-responsive center-block" src="img/img_mug.png" height="266" width="266" alt="">
+            <figcaption>マグカップ</figcaption>
+          </figure>
+        </div>
+        <div class="col-sm-4">
+         <figure>
+            <img class="img-responsive center-block" src="img/img_shirts.png" height="266" width="266" alt="">
+            <figcaption>Tシャツ</figcaption>
+          </figure>
+        </div>
+        <div class="col-sm-4 ">
+          <figure>
+            <img class="img-responsive center-block" src="img/img_seal.png" height="266" width="266" alt="">
+            <figcaption>ステッカー</figcaption>
+          </figure>
+        </div>
+      </div>
+      <h3 class="tit-sub">注意事項</h3>
+      <ul class="notice">
+        <li>グッズは先着順となります。<br>欲しいグッズがある方は、お早めに「企画カウンター」までお越しください。</li>
+        <li>グッズ交換は、おひとり様1回限りです。</li>
+      </ul>
+    </div>
+
+    <footer class="footer">
+        <div class="text-center">
+          <small>WordCamp Kansai 2016</small>
+        </div>
+    </footer>
+  </body>
+</html>
