d3a8b9b256ef42e07d229d07cc83acbb0279a72f
matsui <matsui52@gmail.com>
2016-07-07 01:38:16 +0900

    add PCver

--- complete/_default.php
+++ complete/_default.php
@@ -1,2 +1,35 @@
-<?php
-  echo 'PC ACCESS';
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Sorry! | WordCamp Kansai 2016</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="./css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <header id="header">
+      <div class="container">
+        <h1><img src="img/sorry.png" width="100%"></h1>
+      </div>
+    </header>
+
+    <div class="container">
+      <p class="text-center">このページはパソコン環境からはご覧になれません。<br>
+      スマートフォンかタブレット端末からアクセスしてください。</p>
+      <p class="text-center">（ ↓ 企画の概要についてはこちら ）<br>
+      <a href="https://2016.kansai.wordcamp.org/ar-stamp-rally/">ARスタンプラリー企画もあるんだよ！会場内のわぷーを探してグッズをもらおう！</a></p>
+    </div>
+
+    <footer class="footer">
+        <div class="text-center">
+          <small>WordCamp Kansai 2016</small>
+        </div>
+    </footer>
+
+  </body>
+</html>
